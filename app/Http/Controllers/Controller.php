41a3e35aa8638f8583bc43030e782c85ef6168b2
cheahour <cheahour.choeng@gmail.com>
2021-11-23 23:50:23 +0700

    remove command code

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,12 +7,6 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as APIBaseController;
 
-/**
- * @OA\Info(
- *    title="Today CRM API Documents",
- *    version="1.0.0",
- * )
- */
 class Controller extends APIBaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
