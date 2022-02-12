f3c1dae7e304a6d567b1b5510517c2ec6b1efbef
cheahour <cheahour.choeng@gmail.com>
2022-02-12 09:51:06 +0700

    fix: rename a controller.

--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,7 @@ use App\Http\Controllers\API\APIPackageController;
 use App\Http\Controllers\API\APIIndustryController;
 use App\Http\Controllers\API\APIPipelineController;
 use App\Http\Controllers\API\APITerritoryController;
-use App\Http\Controllers\API\APIKpiActivityController;
+use App\Http\Controllers\API\APIKPIActivityController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -38,7 +38,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('packages', APIPackageController::class);
     Route::apiResource('industries', APIIndustryController::class);
     Route::apiResource('territories', APITerritoryController::class);
-    Route::apiResource('kpi-activities', APIKpiActivityController::class);
+    Route::apiResource('kpi-activities', APIKPIActivityController::class);
     Route::apiResource('pipelines', APIPipelineController::class);
 });
 
